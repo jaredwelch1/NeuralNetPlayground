8af964502a183e2a9305207fc7c1c6976bc5a26f
Jared Welch <welchjb@ngd-11.cgi.missouri.edu>
2018-02-19 14:32:46 -0600

    add data retrieval script

---
+++ resnet/cifar10_download_extract.py
@@ -0,0 +1,43 @@
+"""Downloads and extracts binary version of CIFAR10 dataset."""
+
+import argparse
+import os
+import sys
+import tarfile
+
+from six.moves import urllib
+import tensorflow as tf
+
+DATA_URL = 'https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--data_dir', type=str, default='/tmp/cifar10_data',
+    help="Directory to download data and extract tarbell"
+)
+
+def main(argv):
+    if not os.path.exists(FLAGS.data_dir):
+        os.makedirs(FLAGS.data_dir)
+
+    filename = DATA_URL.split('/')[-1]
+    filepath = os.path.join(FLAGS.data_dir, filename)
+
+    if not os.path.exists(filepath):
+        def _progress(count, block_size, total_size):
+            sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+                filename, 100.0 * count * block_size / total_size
+            ))
+            sys.stdout.flush()
+
+        filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+        print()
+        statinfo = os.stat(filepath)
+        print('Downloaded ', filename, statinfo.st_size, 'bytes successfully.')
+
+    tarfile.open(filepath, 'r:gz').extractall(FLAGS.data_dir)
+
+if __name__ == '__main__':
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(argv=[sys.argv[0]] + unparsed)
